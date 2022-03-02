2eec380cbf0622698746b9431aaa59d9515e4c03
azimut <azimut.github@protonmail.com>
2022-03-02 04:13:05 -0300

    working LTK, but my X server crashed while closing the window
    something similar happened to me with blender, it might be some Intel
    bs but I might take the oportunity to move to a different GUI toolkit

--- ui.lisp
+++ ui.lisp
@@ -17,10 +17,13 @@
     (dotimes (i (length paths) buttons)
       (let ((btn (to-button (nth i paths))))
         (push btn buttons)
+        (bind btn "<FocusOut>" (lambda (ev) (setf (value btn) nil)))
+        (bind btn "<FocusIn>" (lambda (ev) (setf (value btn) t)))
+        (bind btn "<Return>" (lambda (ev) (funcall (command btn))))
         (grid btn 0 i :padx 10 :pady 10)))))
 
 (defun to-button (path &aux (image (image-load (make-image) path)))
   (make-instance
-   'button
+   'check-button
    :image image
    :command (lambda () (print path) (exit-wish))))
